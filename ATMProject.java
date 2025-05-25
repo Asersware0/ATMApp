2aac3e05c53f982244e98c7687aec0e755c1e471
Asersware <103775242+Asersware0@users.noreply.github.com>
2025-05-25 15:52:12 +0300

    Update ATMProject.java

--- ATMProject.java
+++ ATMProject.java
@@ -3,74 +3,90 @@ import java.util.Scanner;
 public class AtmProject {
 
 	public static void main(String[] args) {
-		int bakiye = 1000, input, deger;
+		
+		double bakiye = 1000, deger;
+		int input;
+		String sifre, correctsifre;
+		correctsifre  = "1234";
 		
 		Scanner scan = new Scanner(System.in);
 		System.out.println("****** Aser Banka Hoş Geldiniz!! ******");
-		System.out.println("Güncel Bakiyeniz: " + bakiye + "TL");
+		System.out.println("Lütfen Şifrenizi giriniz: ");
+		sifre = scan.nextLine();
+			
 		
-		while(bakiye >= 0 ) {
-			System.out.println(" ");
-			System.out.println("1- Para Çek");
-			System.out.println("2- Para Yatır");
-			System.out.println("3- Bakiye Sorgula");
-			System.out.println("4- Çıkış Yap");
-			System.out.println(" ");
-			System.out.println("Lütfen işlem seçiniz. (1-4 arası sayı girşi yapınız)");
-			input = scan.nextInt();
+		if(sifre.equals(correctsifre))
+			
+		{
+			
+			System.out.println("**** Giriş Başarılı****");
+			System.out.println("Güncel Bakiyeniz: " + bakiye + "TL");
+		
+			while(bakiye >= 0 ) {
+				System.out.println(" ");
+				System.out.println("1- Para Çek");
+				System.out.println("2- Para Yatır");
+				System.out.println("3- Bakiye Sorgula");
+				System.out.println("4- Çıkış Yap");
+				System.out.println(" ");
+				System.out.println("Lütfen işlem seçiniz. (1-4 arası sayı girşi yapınız)");
+				input = scan.nextInt();
 
 
 			//para çekme işlemi
-			if(input == 1){
-				System.out.println("Lütfen çekmek istediğiniz miktarı giriniz: ");
-				deger= scan.nextInt();
-				if(deger > bakiye) {
-					System.out.println("Bakiye yetersiz!!!");
+				if(input == 1){
+					System.out.println("Lütfen çekmek istediğiniz miktarı giriniz: ");
+					deger= scan.nextDouble();
+					if(deger > bakiye) {
+						System.out.println("Bakiye yetersiz!!!");
+					}
+					else {
+						bakiye = bakiye - deger;
+						System.out.println("İşlem Tamamlandı.... Güncel Bakiyeniz: " + bakiye);
+					}
 				}
-				else {
-					bakiye = bakiye - deger;
-					System.out.println("İşlem Tamamlandı.... Güncel Bakiyeniz: " + bakiye);
-				}
-			}
 			
 			
 			// para yatırma işlemi
-			else if(input == 2) {
+				else if(input == 2) {
 				
 				
-				System.out.println("Lütfen çekmek istediğiniz miktarı giriniz: ");
+					System.out.println("Lütfen çekmek istediğiniz miktarı giriniz: ");
 				
-				deger= scan.nextInt();
-				bakiye = bakiye + deger;
-				System.out.println("İşlem Tamamlandı... Güncek Bakiyeniz: " + bakiye);			
+					deger= scan.nextDouble();
+					bakiye = bakiye + deger;
+					System.out.println("İşlem Tamamlandı... Güncek Bakiyeniz: " + bakiye);			
 				
-			}
+				}
 			
 
 
 			// bakiye sorgulama işlemi
-			else if(input == 3) {
-				System.out.println("Güncel Bakiyeniz. " + bakiye);
-			}
+				else if(input == 3) {
+					System.out.println("Güncel Bakiyeniz. " + bakiye);
+				}
 			
 			
 			// sistemden çıkış işlemi
-			else if(input == 4) {
-				System.out.println("Başarıyla Çıkış yaptınız");
-				break;
-			}
+				else if(input == 4) {
+					System.out.println("Başarıyla Çıkış yaptınız");
+					break;
+				}
 
 			//geçersiz sayı girilince ekranda gözüken yazı
-			else{
-				System.out.println("Lütfen geçerli bir işlem seçiniz....");
+				else{
+					System.out.println("Lütfen geçerli bir işlem seçiniz....");
+				}
+			
+			
+			
 			}
-			
-			
-			
+			System.out.println(" ");
+			System.out.print("İyi Günler Yine Bekleriz \n **************************** ");
 		}
-		System.out.println(" ");
-		System.out.print("İyi Günler Yine Bekleriz \n **************************** ");
-		
+		else {
+			System.out.println("Şifreniz yanlış!!! Lütfen tekrar deneyiniz");
+		}
 	}
 
 }
